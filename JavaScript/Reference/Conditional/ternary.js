13ae90c6660d40d237c15c0ac42d6d7bf5d0b49f
duivvv <geoffrey.dhuyvetters@gmail.com>
2016-08-16 11:06:06 +0200

    changes to ternary

--- JavaScript/Reference/Conditional/ternary.js
+++ JavaScript/Reference/Conditional/ternary.js
@@ -1,8 +1,13 @@
 // 📌 Ternary if/else clause
 
 let bool = true;
-
 let result = bool ? 'Yep' : 'Nope';
 
 console.log(result);
 // 📋 'Yep'
+
+bool = false;
+result = bool ? 'Yep' : 'Nope';
+
+console.log(result);
+// 📋 'Nope'
