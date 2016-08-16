b2d630651116cd5c54a6ba438e604e750a4ee052
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2016-08-16 10:27:09 +0200

    Create ternary.js

---
+++ JavaScript/Reference/Conditional/ternary.js
@@ -0,0 +1,13 @@
+// 📌 Ternary if/else clause
+
+let result;
+
+let statement;
+statement ? result = 'Yep' : result = 'Nope'
+console.log(result);
+// 📋 'Yep'
+
+statement = false;
+statement ? result = 'Yep' : result = 'Nope'
+console.log(result);
+// 📋 'Nope'
