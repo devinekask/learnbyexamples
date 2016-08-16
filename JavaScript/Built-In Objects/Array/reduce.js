a498c098979222a4985df4c6bfb4cc1fa1a08bb8
duivvv <geoffrey.dhuyvetters@gmail.com>
2016-08-16 10:39:43 +0200

    extra info on reduce

--- JavaScript/Built-In Objects/Array/reduce.js
+++ JavaScript/Built-In Objects/Array/reduce.js
@@ -9,7 +9,17 @@ let arr = [1, 2, 3, 4];
 console.log(arr);
 // 📋 [1, 2, 3, 4]
 
-arr = arr.reduce((prev, curr) => prev + curr);
+// 💬 in handler function:
+// 💬 1st argument = value previously returned in the last invocation of the callback
+// 💬 2nd argument = current value in the Array
+// 💬 3rd argument = Array
+
+arr = arr.reduce((prev, curr, arr) => prev + curr);
+
+// 💬 in loop
+// 💬 (1 + 2) = 3
+// 💬 (3 + 3) = 6
+// 💬 (6 + 4) = 10
 
 console.log(arr);
 // 📋 10
